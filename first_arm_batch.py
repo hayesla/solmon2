f19295edb6c3a6a2d11f8bab5f7991ef6a11fccb
admin <admin@guest-142.wireless.ap.dias.ie>
2019-01-30 17:01:20 +0000

    first arm batch attempt

---
+++ first_arm_batch.py
@@ -0,0 +1,64 @@
+import os 
+import sys
+import datetime
+import numpy as np
+#get path access to code in different folders
+sys.path.append('/Users/admin/Documents/solarmonitor_2_0/sol_mon/get_fits')
+sys.path.append('/Users/admin/Documents/solarmonitor_2_0/sol_mon/get_forecasts')
+sys.path.append('/Users/admin/Documents/solarmonitor_2_0/sol_mon/get_data_tests')
+sys.path.append('/Users/admin/Documents/solarmonitor_2_0/sol_mon/get_pngs')
+
+from data_batch import get_summary
+from get_forecast import get_mcstat_forecast, get_mcevol_forecast, get_noaa_probs
+from get_goes_png_test import get_goes_plots
+from get_ace_png_test import get_ace_pngs
+from get_eve_png_test import get_eve_pngs
+
+#--------------------------------------------#
+#date of interest
+date_search = datetime.datetime.strptime('2017-09-10 19:30', '%Y-%m-%d %H:%M')
+#date_search = datetime.datetime.utcnow()
+
+#make directories to store data
+output_path = '/Users/admin/Documents/solarmonitor_2_0/sol_mon/data/'+date_search.strftime('%Y/%m/%d/')
+png_path = output_path + 'pngs/'
+fits_path = output_path + 'fits/'
+meta_path = output_path + 'meta/'
+for i in [png_path, fits_path, meta_path]:
+	if not os.path.exists(i):
+		os.makedirs(i)
+
+#--------------------------------------------#
+
+summary = get_summary(date_search)
+
+
+#get forecasts and add to summary dataframe
+#--------------------------------------------#
+mc_evol = []
+mc_stat = []
+for i in range(len(summary)):
+	me = get_mcevol_forecast(summary['MCINTOSH_TODAY'][i], summary['MCINTOSH_YDAY'][i])
+	ms = get_mcstat_forecast(summary['MCINTOSH_TODAY'][i])
+
+	mc_evol.append(me)
+	mc_stat.append(ms)
+
+
+#this will cause issues
+'''noaa_names, noaa_data = get_noaa_probs()
+noaa_name, noaa_probs = [], []
+for i in range(len(noaa_names)):
+	noaa_name.append(noaa_names[i][0])
+	noaa_probs.append(noaa_data[i])
+
+
+summary['FORE_MCSTAT'] = mc_stat
+summary['FORE_MCEVOL'] = mc_evol
+#summary['FORE_NOAA'] = noaa_probs'''
+
+#-------------------------------------------#
+
+get_goes_plots(date_search, png_path)
+get_ace_pngs(date_search, png_path)
+get_eve_pngs(date_search, png_path)
